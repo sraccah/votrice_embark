4366c04410c6f253e1a3376ac0232e38d9257e8c
sraccah <sraccah@student.42.fr>
2017-10-26 00:31:54 +0200

    op code error vote function

--- dist/js/app.js
+++ dist/js/app.js
@@ -62676,33 +62676,35 @@ var addToLog = function(txt) {
     $(".logs").append("<br>" + txt);
   };
 
-
 $(document).ready(function() {
     // button vote
   	$("button.vote").click(function() {
-		var value = parseInt($("input.text").val(), 1);
+        addToLog(parseInt($("input.text").val()));
 		// If web3.js 1.0 is being used
 		if (EmbarkJS.isNewWeb3()) {
-            Votrice.methods.vote(value).send({from: web3.eth.defaultAccount});
+            Votrice.methods.vote(parseInt($("input.text").val())).send({from: web3.eth.defaultAccount});
             addToLog("Votrice.methods.vote(value).send({from: web3.eth.defaultAccount})");
 		} else {
-            Votrice.vote(value);
+            Votrice.vote(parseInt($("input.text").val()));
             addToLog("Votrice.vote(" + value + ")");
 		}
-      });
+    });
     // button get
   	$("button.get").click(function() {
-		// If web3.js 1.0 is being used
-		if (EmbarkJS.isNewWeb3()) {
+        // If web3.js 1.0 is being used
+        if (EmbarkJS.isNewWeb3()) {
             Votrice.methods.getWinningChoice().call(function(err, value) {
+                if (err) {
+                    addToLog(error);
+                }
                 $(".value").html(value);
-		});
-        addToLog("Votrice.methods.getWinningChoice(console.log)");
+            });
+            addToLog("Votrice.methods.getWinningChoice(console.log)");
 		} else {
             Votrice.getWinningChoice().then(function(value) {
                 $(".value").html(value.toNumber());
-		});
-		addToLog("Votrice.getWinningChoice()");
+		    });
+		    addToLog("Votrice.getWinningChoice()");
 		}
   	});
 });
