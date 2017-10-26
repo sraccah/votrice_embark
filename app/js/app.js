388f022b9f397f42062881056862efed25480ca5
sraccah <sraccah@student.42.fr>
2017-10-26 13:31:08 +0200

    console explicit

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,32 @@
 // add logs
 var addToLog = function(txt) {
-    $(".logs").append(txt + "<br>");
+    $(".logs").append(txt);
   };
 
 $(document).ready(function() {
     // button set
   	$("button.set").click(function() {
-        addToLog(parseInt($("input.set").val()));
 		// If web3.js 1.0 is being used
 		if (EmbarkJS.isNewWeb3()) {
             Votrice.methods.setChoices(parseInt($("input.set").val())).send({from: web3.eth.defaultAccount});
-            addToLog("Votrice.methods.setChoices(value).send({from: web3.eth.defaultAccount})");
+            addToLog("Nombre de projets set (web3) : ");
 		} else {
             Votrice.setChoices(parseInt($("input.set").val()));
-            addToLog("Votrice.setChoices(value)");
+            addToLog("Nombre de projets set : ");
 		}
+        addToLog(parseInt($("input.set").val())+"<br>");
     });
     // button vote
   	$("button.vote").click(function() {
-        addToLog(parseInt($("input.vote").val()));
 		// If web3.js 1.0 is being used
 		if (EmbarkJS.isNewWeb3()) {
             Votrice.methods.vote(parseInt($("input.vote").val())).send({from: web3.eth.defaultAccount});
-            addToLog("Votrice.methods.vote(value).send({from: web3.eth.defaultAccount})");
+            addToLog("A voté ! (web3) : ");
 		} else {
             Votrice.vote(parseInt($("input.vote").val()));
-            addToLog("Votrice.vote(value)");
+            addToLog("A voté ! : ");
 		}
+        addToLog(parseInt($("input.vote").val())+"<br>");
     });
     // button get
   	$("button.get").click(function() {
@@ -36,14 +36,14 @@ $(document).ready(function() {
                 if (err) {
                     addToLog(error);
                 }
-                $(".value").html(value);
+                $(".value").html(Number(value));
             });
-            addToLog("Votrice.methods.getWinningChoice(console.log)");
+            addToLog("Vainqueur demandé (web3) <br>");
 		} else {
             Votrice.getWinningChoice().then(function(value) {
-                $(".value").html(value.toNumber());
+                $(".value").html(Number(value));
 		    });
-		    addToLog("Votrice.getWinningChoice()");
+		    addToLog("Vainqueur demandé <br>");
 		}
   	});
 });
