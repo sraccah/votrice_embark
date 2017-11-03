cf5731fa3cb9095a1935f8b03d1bf6b816b317c2
sraccah <sraccah@student.42.fr>
2017-11-03 19:13:04 +0100

    test done

--- test/contract_spec.js
+++ test/contract_spec.js
@@ -1,27 +1,29 @@
+var assert = require('assert');
+var Embark = require('embark');
+var EmbarkSpec = Embark.initTests();
+var web3 = EmbarkSpec.web3;
+
 describe("Votrice", function() {
- before(function(done) {
-   this.timeout(0);
-   var contractsConfig = {
-     "Votrice": {
-       args: [100, '0x123']
-     }
-   };
-   EmbarkSpec.deployAll(contractsConfig, done);
- });
+    before(function(done) {
+        this.timeout(0);
+        var contractsConfig = {
+            "Votrice":{}
+        };
+        EmbarkSpec.deployAll(contractsConfig, done);
+    });
 
- it("should set constructor value", function(done) {
-   Votrice.storedData(function(err, result) {
-     assert.equal(result.toNumber(), 100);
-     done();
-   });
- });
-
- it("set storage value", function(done) {
-   Votrice.vote(150, function() {
-     Votrice.getWinningChoice(function(err, result) {
-       assert.equal(result.toNumber(), 150);
-       done();
-     });
-   });
- });
+    it("set storage value", function(done) {
+        Votrice.addProject("name", function() {
+            Votrice.getWinningChoice(function(err, result) {
+                assert.equal("name");
+                done();
+            });
+        });
+        Votrice.vote(150, function() {
+            Votrice.getWinningChoice(function(err, result) {
+                assert.equal(result.toNumber(), 150);
+                done();
+            });
+        });
+    });
 });
