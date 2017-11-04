1d882d6269cb5e26e72ad185cb4ee91aad545139
sraccah <sraccah@student.42.fr>
2017-11-05 00:24:34 +0100

    names added

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@ var addToConsole = function(txt) {
     $(".logs").append(txt);
 };
 
+
 // DAPP
 $(document).ready(function() {
     // all projects names
@@ -13,20 +14,21 @@ $(document).ready(function() {
     var winners = [];
     // all projects addresses
     var adresses = [];
+    // get all projects and display them with the possibility to vote for them
     if (projects_count == 0) {
         $(".allprojects").html("<div>Project : "+projects[projects_count]+" : "+(projects_count+1)+"<button class=\"vote btn btn-default\" value=\""+projects_count+"\">Voter pour ce projet</button>"+"</div>");
     } else {
-        $(".allprojects").append("<div>Project "+projects[projects_count]+" : "+(projects_count+1)+"<button class=\"vote btn btn-default\" value=\""+projects_count+"\">Voter pour ce projet</button>"+"</div>");
+        $(".allprojects").append("<div>Project : "+projects[projects_count]+" : "+(projects_count+1)+"<button class=\"vote btn btn-default\" value=\""+projects_count+"\">Voter pour ce projet</button>"+"</div>");
     }
     // button set
   	$("button.set").click(function() {
         Votrice.methods.getSenderAddress().call().then((caller) => {
             adresses[projects_count] = caller;
             if (EmbarkJS.isNewWeb3()) {
-                Votrice.methods.didAdd(caller).call((value) => {
+                Votrice.methods.didAdd(caller).call().then((value) => {
                     if (value == false) {
-                        Votrice.methods.addChoice($("input.set").val()).send({from: value});
-                        addToConsole("Projet ajouté (web3) : " + $("input.set").val() + "<br>address : " + value + "<br>");
+                        Votrice.methods.addChoice($("input.set").val()).send({from: caller});
+                        addToConsole("Projet ajouté (web3)<br>name : " + $("input.set").val() + "<br>address : " + caller + "<br>");
                     } else {
                         addToConsole("A déjà ajouter son projet ! (web3) : ACTION ANNULÉE <br>");
                     }
@@ -35,7 +37,7 @@ $(document).ready(function() {
                 Votrice.methods.didAdd(caller).call((value) => {
                     if (value == false) {
                         Votrice.addChoice($("input.set").val());
-                        addToConsole("Projet ajouté : " + $("input.set").val() + "<br>address : " + value + "<br>");
+                        addToConsole("Projet ajouté<br>name : " + $("input.set").val() + "<br>address : " + value + "<br>");
                     } else {
                         addToConsole("A déjà ajouter son projet ! : ACTION ANNULÉE <br>");
                     }
@@ -75,21 +77,29 @@ $(document).ready(function() {
         $(".winners").empty();
         if (EmbarkJS.isNewWeb3()) {
             Votrice.methods.getWinners().call().then((value) => {
-                addToConsole("Vainqueurs demandé (web3) : ");
-                value.forEach((element) => {
-                    var nbr = Number(element) + 1;
-                    $(".winners").append("<p>Projet " + nbr + "</p>");
-                }, this);
-                addToConsole(value + "<br>");
+                if (value.length) {
+                    addToConsole("Vainqueurs demandé (web3) : ");
+                    value.forEach((element) => {
+                        var nbr = Number(element) + 1;
+                        $(".winners").append("<p>Projet " + nbr + "</p>");
+                    }, this);
+                    addToConsole(value + "<br>");
+                } else {
+                    addToConsole("No winners yet !<br>");
+                }
             });
 		} else {
             Votrice.getWinners().then((value) => {
-                addToConsole("Vainqueurs demandé : ");
-                value.forEach((element) => {
-                    var nbr = Number(element) + 1;
-                    $(".winners").append("<p>Projet " + nbr + "</p>");
-                }, this);
-                addToConsole(value + "<br>");
+                if (value) {
+                    addToConsole("Vainqueurs demandé : ");
+                    value.forEach((element) => {
+                        var nbr = Number(element) + 1;
+                        $(".winners").append("<p>Projet " + nbr + "</p>");
+                    }, this);
+                    addToConsole(value + "<br>");
+                } else {
+                    addToConsole("No winners yet !<br>");
+                }
 		    });
         }
     });
